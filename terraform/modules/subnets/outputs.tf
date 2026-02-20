3d1712409db5abd49ed1cced608ccf471a2bd7c9
Jenom <119628596+shehuj@users.noreply.github.com>
2026-02-20 11:40:25 -0600

    Update outputs.tf

--- terraform/modules/subnets/outputs.tf
+++ terraform/modules/subnets/outputs.tf
@@ -1,9 +1,11 @@
 output "public_subnet_id" {
-  value       = aws_subnet.public_subnet.id
+#  value       = aws_subnet.public_subnet.id
+  value       = aws_subnet.public_subnet[count.index]
   description = "The ID of the public subnet"
 }
 
 output "private_subnet_id" {
-  value       = aws_subnet.private_subnet.id
+#  value       = aws_subnet.private_subnet.id
+  value       = aws_subnet.private_subnet[count.index]
   description = "The ID of the private subnet"
 }
