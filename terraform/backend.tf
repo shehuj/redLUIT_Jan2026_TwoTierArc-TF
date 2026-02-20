be68dd497a0e15100a7f4193e7c63f5f3830a897
Jenom <119628596+shehuj@users.noreply.github.com>
2026-02-19 19:52:22 -0600

    initial commit

---
+++ terraform/backend.tf
@@ -0,0 +1,11 @@
+# terraform backend configuration for remote state storage in AWS S3 and state locking with DynamoDB
+
+terraform {
+    backend "s3" {
+        bucket         = "ec2-shutdown-lambda-bucket"
+        key            = "two-tier-arc/terraform.tfstate"
+        region         = "us-east-1"
+        dynamodb_table = "dyning_table"
+        encrypt        = true
+    }
+    }   
